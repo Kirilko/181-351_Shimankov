48657f1045e79d809e71d54093a01dafab3a7e2f
g.force00@mail.ru <chipzz87@gmail.com>
2018-09-23 18:41:42 +0400

    23.09.2018 18:41:42,06
    Signed-off-by: g.force00@mail.ru <chipzz87@gmail.com>

--- 123.bat
+++ 123.bat
@@ -1,4 +1,10 @@
-git add *.cpp *.h *.sln *.vcxproj *.user *.filters
-git commit -am "Some comment"
-git push origin master
+git add *.cpp 
+git add *.sln 
+git add *.vcxproj 
+git add *.vcxproj.filters 
+git add *.h 
+git add *.bat 
+git add *.user 
+git commit -sm "%DATE% %TIME%" 
+git push origin master 
 pause
